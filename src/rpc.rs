72e096171c14151e2c251cba8a348164f03f66f5
husky <husky.robot.dog@gmail.com>
2024-03-26 21:49:25 +0800

    coding

--- src/rpc.rs
+++ src/rpc.rs
@@ -1,132 +1,82 @@
-mod rpc {
-    tonic::include_proto!("starfish");
-}
+use crate::env;
 use anyhow::anyhow;
-use prost_wkt_types::Any;
+use futures::Future;
+use inner::{
+	starfish_client::StarfishClient,
+	starfish_server::{Starfish, StarfishServer},
+};
+pub use inner::{StarfishCode, StarfishResult};
+use prost_wkt_types::{Any, MessageSerde};
 use std::{net::ToSocketAddrs, pin::Pin};
 use tokio::sync::mpsc::{channel, Receiver};
 use tokio_stream::{wrappers::ReceiverStream, Stream, StreamExt};
 use tonic::{
-    transport::{Channel, Server},
-    Request, Response, Status, Streaming,
+	transport::{Channel, Server},
+	Request, Response, Status, Streaming,
 };
 
-use crate::env;
-
-use self::rpc::api_client::ApiClient;
-
-#[async_trait::async_trait]
-pub trait ServerHandle {
-    async fn call(&self, request: Any) -> anyhow::Result<rpc::Result>;
-    async fn subscribe(&self, request: Any) -> anyhow::Result<Receiver<rpc::Result>>;
-    async fn channel(&self, request: Receiver<Any>) -> anyhow::Result<Receiver<rpc::Result>>;
-}
-
-struct RpcServer {
-    handle: Box<dyn ServerHandle + Send + Sync>,
-}
-
-impl RpcServer {
-    pub fn new(handle: Box<dyn ServerHandle + Send + Sync>) -> Self {
-        Self { handle }
-    }
+mod inner {
+	tonic::include_proto!("starfish");
 }
 
 #[async_trait::async_trait]
-impl rpc::api_server::Api for RpcServer {
-    type SubscribeStream = Pin<Box<dyn Stream<Item = Result<rpc::Result, Status>> + Send>>;
-    type ChannelStream = Pin<Box<dyn Stream<Item = Result<rpc::Result, Status>> + Send>>;
+pub trait CallHandle: Send + Sync + 'static {
+	type Request: MessageSerde + Default;
 
-    async fn call(&self, request: Request<Any>) -> Result<Response<rpc::Result>, Status> {
-        let result = self
-            .handle
-            .call(request.into_inner())
-            .await
-            .map_err(|err| Status::new(tonic::Code::Internal, err.to_string()))?;
-        Ok(Response::new(result))
-    }
+	async fn call(&self, request: Self::Request) -> anyhow::Result<StarfishResult>;
 
-    async fn subscribe(
-        &self,
-        request: Request<Any>,
-    ) -> Result<Response<Self::SubscribeStream>, Status> {
-        let mut result = self
-            .handle
-            .subscribe(request.into_inner())
-            .await
-            .map_err(|err| Status::new(tonic::Code::Internal, err.to_string()))?;
-        let (tx, rx) = channel(1024);
-        tokio::spawn(async move {
-            while let Some(item) = result.recv().await {
-                if tx.send(Ok(item)).await.is_err() {
-                    break;
-                }
-            }
-            tx.closed().await;
-        });
-        Ok(Response::new(
-            Box::pin(ReceiverStream::new(rx)) as Self::SubscribeStream
-        ))
-    }
-
-    async fn channel(
-        &self,
-        request: Request<Streaming<Any>>,
-    ) -> Result<Response<Self::ChannelStream>, Status> {
-        let (tx, rx) = channel(1024);
-        let mut request = request.into_inner();
-        let _tx = tx.clone();
-        tokio::spawn(async move {
-            while let Some(item) = request.next().await {
-                if let Ok(item) = item {
-                    if _tx.send(item).await.is_err() {
-                        break;
-                    }
-                }
-            }
-            _tx.closed().await;
-        });
-        let mut result = self
-            .handle
-            .channel(rx)
-            .await
-            .map_err(|err| Status::new(tonic::Code::Internal, err.to_string()))?;
-        let (tx, rx) = channel(1024);
-        tokio::spawn(async move {
-            while let Some(item) = result.recv().await {
-                if tx.send(Ok(item)).await.is_err() {
-                    break;
-                }
-            }
-            tx.closed().await;
-        });
-        Ok(Response::new(
-            Box::pin(ReceiverStream::new(rx)) as Self::SubscribeStream
-        ))
-    }
+	fn get_key() -> &'static str {
+		Self::Request::default().type_url()
+	}
 }
 
-pub async fn init_server(
-    config: &env::Rpc,
-    handle: Box<dyn ServerHandle + Send + Sync>,
-) -> anyhow::Result<()> {
-    let rpc_server = RpcServer::new(handle);
+#[async_trait::async_trait]
+pub trait SubscribeHandle: Send + Sync + 'static {
+	type Request: MessageSerde + Default;
 
-    Server::builder()
-        .add_service(rpc::api_server::ApiServer::new(rpc_server))
-        .serve(
-            config
-                .bind
-                .to_socket_addrs()?
-                .next()
-                .ok_or(anyhow!("parse address failed : {}", config.bind))?,
-        )
-        .await?;
+	async fn call(&self, request: Self::Request) -> anyhow::Result<Receiver<StarfishResult>>;
 
-    Ok(())
+	fn get_key() -> &'static str {
+		Self::Request::default().type_url()
+	}
 }
 
-pub async fn init_client(url: String) -> anyhow::Result<ApiClient<Channel>> {
-    let client = ApiClient::connect(url).await?;
-    Ok(client)
+#[async_trait::async_trait]
+pub trait StreamHandle: Send + Sync + 'static {
+	type Request: MessageSerde + Default;
+
+	async fn call(
+		&self,
+		request: Receiver<Self::Request>,
+	) -> anyhow::Result<Receiver<StarfishResult>>;
+
+	fn get_key() -> &'static str {
+		Self::Request::default().type_url()
+	}
+}
+
+struct StarfishImpl {}
+
+#[async_trait::async_trait]
+impl Starfish for StarfishImpl {
+	type StreamStream = Pin<Box<dyn Stream<Item = Result<StarfishResult, Status>> + Send>>;
+	type SubscribeStream = Pin<Box<dyn Stream<Item = Result<StarfishResult, Status>> + Send>>;
+
+	async fn call(&self, request: Request<Any>) -> Result<Response<StarfishResult>, Status> {
+		todo!()
+	}
+
+	async fn subscribe(
+		&self,
+		request: Request<Any>,
+	) -> Result<Response<Self::SubscribeStream>, Status> {
+		todo!()
+	}
+
+	async fn stream(
+		&self,
+		request: Request<Streaming<Any>>,
+	) -> Result<Response<Self::SubscribeStream>, Status> {
+		todo!()
+	}
 }
