7d7a3149a3dfaa5f6ad6614b82ecdcc8fc6f5c84
husky <husky.robot.dog@gmail.com>
2024-03-26 21:02:18 +0800

    coding

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,17 @@
 #[cfg(feature = "encrypt")]
 pub mod encrypt;
-
 #[cfg(feature = "env")]
 pub mod env;
-
 #[cfg(feature = "http")]
 pub mod http;
-
 #[cfg(feature = "log")]
 pub mod log;
-
 #[cfg(feature = "orm")]
 pub mod orm;
-
-#[cfg(feature = "uuid")]
-pub mod uuid;
-
-#[cfg(feature = "web")]
-pub mod web;
-
 #[cfg(feature = "rpc")]
 pub mod rpc;
-
 pub mod runtime;
+#[cfg(feature = "uuid")]
+pub mod uuid;
+#[cfg(feature = "web")]
+pub mod web;
