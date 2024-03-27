7a4130d49d155f5793a0ff7cd0d0442a28209f1d
husky <husky.robot.dog@gmail.com>
2024-03-27 20:28:38 +0800

    coding

--- src/env.rs
+++ src/env.rs
@@ -1,64 +1,49 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, Default, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Log {
-    #[serde(default = "default_log_level")]
-    pub level: String,
-    #[serde(default = "default_log_console")]
-    pub console: bool,
-    #[serde(default)]
-    pub file: bool,
+	#[serde(default = "default_log_level")]
+	pub level: String,
+	#[serde(default = "default_log_console")]
+	pub console: bool,
+	#[serde(default)]
+	pub to_file: bool,
 }
-
 fn default_log_level() -> String {
-    "info".into()
+	"info".into()
 }
-
 fn default_log_console() -> bool {
-    true
+	true
 }
 
-#[derive(Debug, Clone, Default, Serialize, Deserialize)]
-pub struct Web {
-    #[serde(default = "default_web_bind")]
-    pub bind: String,
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Database {
+	pub host: String,
+	pub port: u32,
+	pub username: String,
+	pub password: String,
+	pub database: String,
+	pub url: String,
+	#[serde(default = "default_orm_max_conn")]
+	pub max_conn: u32,
+	pub show_sql: bool,
 }
-fn default_web_bind() -> String {
-    "0.0.0.0:10000".into()
-}
-
-#[derive(Debug, Clone, Default, Serialize, Deserialize)]
-pub struct Orm {
-    #[serde(default)]
-    pub url: String,
-    #[serde(default = "default_orm_max_conn")]
-    pub max_conn: u32,
-    #[serde(default)]
-    pub show_sql: bool,
-}
-
 fn default_orm_max_conn() -> u32 {
-    num_cpus::get() as u32
+	(num_cpus::get() * 2) as u32
 }
 
-#[derive(Debug, Clone, Default, Serialize, Deserialize)]
-pub struct Rpc {
-    #[serde(default = "default_rpc_bind")]
-    pub bind: String,
-}
-fn default_rpc_bind() -> String {
-    "0.0.0.0:10000".into()
-}
-
-pub fn init<'de, T: Deserialize<'de>>() -> anyhow::Result<T> {
-    let settings = config::Config::builder()
-        .add_source(config::File::with_name("config.toml").required(false))
-        .add_source(
-            config::Environment::with_prefix("starfish")
-                .try_parsing(true)
-                .separator("__"),
-        )
-        .build()?;
-    let config: T = settings.try_deserialize()?;
-    Ok(config)
+pub fn init<'de, T: Deserialize<'de>>(
+	config_name: Option<&str>,
+	prefix: Option<&str>,
+) -> anyhow::Result<T> {
+	let settings = config::Config::builder()
+		.add_source(config::File::with_name(config_name.unwrap_or("config.toml")).required(false))
+		.add_source(
+			config::Environment::with_prefix(prefix.unwrap_or("starfish"))
+				.try_parsing(true)
+				.separator("__"),
+		)
+		.build()?;
+	let config: T = settings.try_deserialize()?;
+	Ok(config)
 }
